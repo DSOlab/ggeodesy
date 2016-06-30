ea2675a62327ef3df70f2d53af0d41d59006a755
xanthos <xanthos@mail.ntua.gr>
2016-06-30 11:20:27 +0300

    dont even know

--- src/vincenty.hpp
+++ src/vincenty.hpp
@@ -1,3 +1,14 @@
+///
+/// \file vincenty.hpp
+///
+/// \brief Implementation of Vincenty's formulae and other algorithms to
+///        compute great circle/godesic quantities.
+///
+/// \todo
+///       - Need to test!
+///       - Implement the direct/inverse algorithms from GographicLib, see
+///         http://geographiclib.sourceforge.net/html/classGeographicLib_1_1Geodesic.html
+
 #ifndef __NGPT_VINCENTY__HPP
 #define __NGPT_VINCENTY_HPP
 
@@ -92,7 +103,7 @@ template<ellipsoid E = ellipsoid::wgs84>
 
     do {
         if (++iteration > MAX_ITERATIONS) {
-            throw std::out_of_range("Inverse Vincenty cannot converge after 1000 iterations!");
+            throw std::out_of_range("Inverse Vincenty cannot converge after 100 iterations!");
         }
         sinLambda  = std::sin(lambda);
         cosLambda  = std::cos(lambda);
@@ -133,11 +144,72 @@ template<ellipsoid E = ellipsoid::wgs84>
 /// s, along the geodesic the problem is to find the end point (lat2, lon2)
 /// and azimuth, a2.
 ///
+/// \warning 
+///
+/// \see https://en.wikipedia.org/wiki/Vincenty%27s_formulae
 template<ellipsoid E>
-    double direct_vincenty(double lat1, double lon1, double a1, double& lat2,
-        double& lon2)
+    double direct_vincenty(double lat1, double lon1, double a1, double s, 
+        double& lat2, double& lon2, double convergence_limit = 1e-10)
 {
-    double U1 { std::atan(
+    const int MAX_ITERATIONS = 100;
+    int iteration = 0;
+
+    double a = ellipsoid_traits<E>::a;
+    double f = ellipsoid_traits<E>::f;
+    double b = semi_minor<E>();
+    
+    double cosa1 { std::cos(a1) };
+    double sina1 { std::sin(a1) };
+    double U1 { std::atan((1-f)*std::tan(lat1)) };
+    double sigma1 { std::atan(std::tan(U1)/cosa1) };
+    double sina { std::cos(U1) * sina1 };
+    double cosaSq {1-sina*sina};
+    double uSq { cosaSq*(a*a-b*b)/(b*b) };
+    double k1 { (std::sqrt(1.0+uSq)-1.0)/(std::sqrt(1.0+uSq)+1.0) };
+    double A  { (1+0.25*k1*k1)/(1.0-k1) };
+    double B  { k1*(1.0-(3.0/8.0)*k1*k1) };
+
+    double sigma { s/b*A }; // initial guess
+    double sigmaP, sigmaM2, cosSigmaM2, deltaSigma, sinSigma;
+
+    do {
+        if (++iteration > MAX_ITERATIONS) {
+            throw std::out_of_range("Direct Vincenty cannot converge after 100 iterations!");
+        }
+        sigmaM2 = 2*sigma1 + sigma;
+        cosSigmaM2 = std::cos(sigmaM2);
+        sinSigma = std::sin(sigma);
+        deltaSigma = B*sinSigma*(cosSigmaM2 
+            + (1/4.0)*B*(std::cos(sigma)*(-1+2*cosSigmaM2*cosSigmaM2)
+            - (1/6.0)*B*cosSigmaM2*(-3+4*sinSigma*sinSigma)*(-3+4*cosSigmaM2*cosSigmaM2)));
+        sigmaP = sigma;
+        sigma = s/b*A + deltaSigma;
+    } while ( std::abs(sigma-sigmaP) > convergence_limit );
+#ifdef DEBUG
+    std::cout<<"\tDirect Vincenty converged after "<<iteration<<" iterations\n";
+#endif
+
+    double sinU1 { std::sin(U1) };
+    double cosU1 { std::cos(U1) };
+    double cosSigma { std::cos(sigma) };
+
+    // compute latitude
+    double nom { sinU1*cosSigma+cosU1*sinSigma*cosa1 };
+    double denom { sina*sina+std::pow(sinU1*sinSigma-cosU1*cosU1*cosSigma*cosa1, 2.0) };
+    denom = std::sqrt(denom)*(1.0-f);
+    lat2 = std::atan2(nom, denom);
+
+    // compute longtitude
+    nom      = sinSigma*sina1;
+    denom    = cosU1*cosSigma-sinU1*sinSigma*cosa1;
+    double lambda { std::atan2(nom, denom) };
+    double C { (f/16.0)*cosaSq(4+f*(4-3*cosaSq)) };
+    double L { lambda + (1.0-C)*f*sina*(sigma+C*sinSigma*(cosSigmaM2+C*cosSigma*
+                (-1.0+2.0*cos2SigmaM*cos2SigmaM))) };
+    lon2     = L + lon1;
+    
+    // compute azimouth
+    return std::atan2(sina, -sinU1*sinSigma+cosU1*cosSigma*cosa1);
 }
 
 } // end namespace ngpt
