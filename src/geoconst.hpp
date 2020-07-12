131ffc7b9be30e3a227b4bce26769b4e2b810e7f
xanthos <xanthos.papanikolaou@gmail.com>
2020-07-12 22:20:11 +0300

    added test for units

--- src/geoconst.hpp
+++ src/geoconst.hpp
@@ -9,27 +9,26 @@
 
 #include <cmath>
 
-namespace ngpt
-{
-  /// The value of pi.
+namespace ngpt {
+/// The value of pi.
 #if defined(__GNUC__) && !defined(__llvm__)
-  constexpr double DPI  {std::atan(1e0)*4e0};
+constexpr double DPI{std::atan(1e0) * 4e0};
 #else
-  #define _USE_MATH_DEFINES
-  constexpr double DPI {M_PI};
+#define _USE_MATH_DEFINES
+constexpr double DPI{M_PI};
 #endif
-  
-  /// The value of 2 * pi.
-  constexpr double D2PI {2e0*DPI};
 
-  /// Degrees to Radians coefficient.
-  constexpr double DEG2RAD {DPI/180e0};
-  
-  /// Radians to Degrees coefficient.
-  constexpr double RAD2DEG {180e0/DPI};
+/// The value of 2 * pi.
+constexpr double D2PI{2e0 * DPI};
 
-  /// mas to radians factor aka θrad = θmas*MAS2RAD
-  constexpr double MAS2RAD {4.847309743e-9}; 
+/// Degrees to Radians coefficient.
+constexpr double DEG2RAD{DPI / 180e0};
+
+/// Radians to Degrees coefficient.
+constexpr double RAD2DEG{180e0 / DPI};
+
+/// mas to radians factor aka θrad = θmas*MAS2RAD
+constexpr double MAS2RAD{4.847309743e-9};
 
 } // namespace ngpt
 
---
+++ test/test_units.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <random>
+#include <cassert>
+#include "geodesy.hpp"
+
+double generate_random_double(double lower_bound, double upper_bound) noexcept {
+  std::random_device rd;
+  std::mt19937 e2(rd());
+  std::uniform_real_distribution<double> unif(lower_bound,upper_bound);
+  return unif(e2);
+}
+
+constexpr double ErrLimit = 1e-13;
+
+int main()
+{
+  double angle_radians,
+         angle_degrees,
+         seconds,
+         theta;
+  int deg, min;
+
+  for (int i=0; i<150; ++i) {
+    // radians to dec. degrees and back
+    angle_radians = generate_random_double(-ngpt::D2PI, ngpt::D2PI);
+    angle_degrees = ngpt::rad2deg(angle_radians);
+    assert(std::abs(ngpt::deg2rad(angle_degrees)-angle_radians)<ErrLimit);
+
+    // dec. degrees to radians and back
+    angle_degrees = generate_random_double(-360e0, 360e0);
+    angle_radians = ngpt::deg2rad(angle_degrees);
+    assert(std::abs(ngpt::rad2deg(angle_radians)-angle_degrees)<ErrLimit);
+
+    // dec. degrees to hexicondal degrees and back
+    angle_degrees = generate_random_double(-360e0, 360e0);
+    ngpt::decd2hexd(angle_degrees, deg, min, seconds);
+    assert(std::abs(ngpt::hexd2decd(deg, min, seconds)-angle_degrees)<ErrLimit);
+    assert(std::abs(ngpt::hexd2rad(deg, min, seconds)
+          -ngpt::deg2rad(angle_degrees))<ErrLimit);
+
+    // radians to hexicondal degrees and back
+    angle_radians = generate_random_double(-ngpt::D2PI, ngpt::D2PI);
+    ngpt::rad2hexd(angle_radians, deg, min, seconds);
+    assert(std::abs(ngpt::hexd2rad(deg, min, seconds)-angle_radians)<ErrLimit);
+    assert(std::abs(ngpt::hexd2decd(deg, min, seconds)
+          -ngpt::rad2deg(angle_radians))<ErrLimit);
+
+    // check the angle normalization
+    angle_radians = generate_random_double(-ngpt::D2PI, ngpt::D2PI);
+    theta = ngpt::normalize_angle(angle_radians, 0e0, ngpt::D2PI);      // [0, 2π)
+    assert(std::abs(std::sin(angle_radians)-std::sin(theta))<ErrLimit);
+    assert(std::abs(std::cos(angle_radians)-std::cos(theta))<ErrLimit);
+    theta = ngpt::normalize_angle(angle_radians, -ngpt::DPI, ngpt::DPI);// [-π, π)
+    assert(std::abs(std::sin(angle_radians)-std::sin(theta))<ErrLimit);
+    assert(std::abs(std::cos(angle_radians)-std::cos(theta))<ErrLimit);
+  }
+
+  return 0;
+}
