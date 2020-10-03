f023fe615d3075f40a84fe7a481fafe51f486277
xanthos <xanthos.papanikolaou@gmail.com>
2020-10-03 19:24:25 +0300

    added source files

---
+++ src/vincenty.cpp
@@ -0,0 +1,317 @@
+#include "vincenty.hpp"
+#include "geoconst.hpp"
+#include <cmath>
+#include <stdexcept>
+
+using ngpt::D2PI;
+using ngpt::DPI;
+
+/// @brief Compute the inverse Vincenty formula.
+///
+/// Given the (ellipsoidal) coordinates of two points (1 and 2) in radians,
+/// calculate the forward azimouths from 1->2 (i.e. a12) and from 2->1 (i.e.
+/// a21) and the ellipsoidal (along the geodesic) distance between the two
+/// points. The inverse Vincenty's formula is used for the computation.
+/// Vincenty’s solution for the distance between points on an ellipsoidal earth
+/// model is accurate to within 0.5 mm distance (!), 0.000015″ bearing, on the
+/// ellipsoid being used. Calculations based on a spherical earth model, such
+/// as the (much simpler) Haversine, are accurate to around 0.3% – which is
+/// still good enough for many (most?) purposes, of course (see
+/// https://www.movable-type.co.uk/scripts/latlong-vincenty.html).
+///
+/// @param[in]  lat1        Latitude of point 1 (radians)
+/// @param[in]  lon1        Longtitude of point 1 (radians)
+/// @param[in]  lat2        Latitude of point 2 (radians)
+/// @param[in]  lon2        Longtitude of point 2 (radians)
+/// @param[in]  semi_major  Semi-major axis of reference ellipsoid (meters)
+/// @param[in]  semi_minor  Semi-minor axis of reference ellipsoid (meters)
+/// @param[in]  flattening  Flattening of reference ellipsoid
+/// @param[out] a12         Azimouth from point 1 to point 2 (radians)
+/// @param[out] a21         Azimouth from point 2 to point 1 (radians)
+/// @param[in]  convergence_limit Convergence limit (radians). 10e−12
+///                         corresponds to approximately 0.06mm
+/// @return      Ellipsoidal (along the geodesic) distance between the two
+///              points (meters).
+///
+/// @throw Will throw an std::out_of_range (implying the input aruments/points)
+///        in case more than MAX_ITERATIONS(=100) are needed for the algorithm
+///        to converge. Solution to the inverse problem fails to converge or
+///        converges slowly for nearly antipodal points.
+///
+/// @see https://en.wikipedia.org/wiki/Vincenty's_formulae
+///
+double ngpt::core::inverse_vincenty(double lat1, double lon1, double lat2,
+                                    double lon2, double semi_major,
+                                    double flattening, double semi_minor,
+                                    double &a12, double &a21,
+                                    double convergence_limit) {
+  const int MAX_ITERATIONS = 100;
+  int iteration = 0;
+
+  const double a = semi_major;
+  const double f = flattening;
+  const double b = semi_minor;
+
+  const double U1{std::atan((1e0 - f) * std::tan(lat1))};
+  const double U2{std::atan((1e0 - f) * std::tan(lat2))};
+  const double L{lon2 - lon1};
+  const double sinU1{std::sin(U1)};
+  const double sinU2{std::sin(U2)};
+  const double cosU1{std::cos(U1)};
+  const double cosU2{std::cos(U2)};
+  double lambda{L};
+  double sinSigma, cosSigma, sigma, sinAlpha, cosSqAlpha, C, cos2SigmaM,
+      lambdaP, sinLambda, cosLambda;
+
+  do {
+    if (++iteration > MAX_ITERATIONS) {
+      throw std::out_of_range(
+          "[ERROR] Inverse Vincenty cannot converge after 100 iterations!");
+    }
+    sinLambda = std::sin(lambda);
+    cosLambda = std::cos(lambda);
+    sinSigma = std::sqrt((cosU2 * sinLambda) * (cosU2 * sinLambda) +
+                         (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda) *
+                             (cosU1 * sinU2 - sinU1 * cosU2 * cosLambda));
+    cosSigma = sinU1 * sinU2 + cosU1 * cosU2 * cosLambda;
+    sigma = atan2(sinSigma, cosSigma);
+    sinAlpha = cosU1 * cosU2 * sinLambda / sinSigma;
+    cosSqAlpha = 1e0 - sinAlpha * sinAlpha;
+    cos2SigmaM = cosSigma - 2e0 * sinU1 * sinU2 / cosSqAlpha;
+    C = (f / 16e0) * cosSqAlpha * (4e0 + f * (4e0 - 3e0 * cosSqAlpha));
+    lambdaP = lambda;
+    lambda = L + (1e0 - C) * f * sinAlpha *
+                     (sigma + C * sinSigma *
+                                  (cos2SigmaM +
+                                   C * cosSigma *
+                                       (-1e0 + 2e0 * cos2SigmaM * cos2SigmaM)));
+  } while (std::abs(lambda - lambdaP) > convergence_limit);
+
+  const double uSq{cosSqAlpha * ((a * a - b * b) / (b * b))};
+  const double k1{(std::sqrt(1e0 + uSq) - 1e0) / (std::sqrt(1e0 + uSq) + 1e0)};
+  const double A{(1e0 + 0.25e0 * k1 * k1) / (1e0 - k1)};
+  const double B{k1 * (1e0 - (3e0 / 8e0) * k1 * k1)};
+  const double deltaSigma{
+      B * sinSigma *
+      (cos2SigmaM +
+       B / 4e0 *
+           (cosSigma * (-1e0 + 2e0 * cos2SigmaM * cos2SigmaM) -
+            B / 6e0 * cos2SigmaM * (-3e0 + 4e0 * sinSigma * sinSigma) *
+                (-3e0 + 4e0 * cos2SigmaM * cos2SigmaM)))};
+  double distance{b * A * (sigma - deltaSigma)};
+
+  // forward azimouth
+  a12 =
+      std::atan2(cosU2 * sinLambda, cosU1 * sinU2 - sinU1 * cosU2 * cosLambda);
+  // normalize
+  a12 = std::fmod(a12 + D2PI, D2PI);
+
+  // backward azimouth
+  a21 =
+      std::atan2(cosU1 * sinLambda, -sinU1 * cosU2 + cosU1 * sinU2 * cosLambda);
+  // normalize
+  a21 = std::fmod(a21 + D2PI, D2PI);
+
+  return distance;
+}
+
+/// @brief Direct Vincenty formula.
+///
+/// Given an initial point (lat1, lon1), an initial azimuth, a1, and a distance,
+/// s, along the geodesic the problem is to find the end point (lat2, lon2)
+/// and azimuth, a2.
+/// Vincenty’s solution for the distance between points on an ellipsoidal earth
+/// model is accurate to within 0.5 mm distance (!), 0.000015″ bearing, on the
+/// ellipsoid being used. Calculations based on a spherical earth model, such
+/// as the (much simpler) Haversine, are accurate to around 0.3% – which is
+/// still good enough for many (most?) purposes, of course (see
+/// https://www.movable-type.co.uk/scripts/latlong-vincenty.html).
+///
+/// @param[in]  lat1        Latitude of point 1 (radians)
+/// @param[in]  lon1        Longtitude of point 1 (radians)
+/// @param[in]  a1          Azimouth from point 1 to point 2 (radians)
+/// @param[in]  s           Ellipsoid distance (along the geodesic) from point
+///                         1 to point 2 (meters)
+/// @param[in]  semi_major  Semi-major axis of reference ellipsoid (meters)
+/// @param[in]  semi_minor  Semi-minor axis of reference ellipsoid (meters)
+/// @param[in]  flattening  Flattening of reference ellipsoid
+/// @param[out] lat2        Latitude of point 2 (radians)
+/// @param[out] lon2        Longtitude of point 2 (radians)
+/// @param[in]  convergence_limit Convergence limit (radians). 10e−12
+///                         corresponds to approximately 0.06mm
+/// @return     Azimouth from point 2 to point 1 (radians)
+///
+/// @see https://en.wikipedia.org/wiki/Vincenty%27s_formulae
+double ngpt::core::direct_vincenty(double lat1, double lon1, double a1,
+                                   double s, double semi_major,
+                                   double flattening, double semi_minor,
+                                   double &lat2, double &lon2,
+                                   double convergence_limit) {
+  const int MAX_ITERATIONS = 100;
+  int iteration = 0;
+
+  double a = semi_major;
+  double f = flattening;
+  double b = semi_minor;
+
+  const double cosa1{std::cos(a1)};
+  const double sina1{std::sin(a1)};
+  const double U1{std::atan((1e0 - f) * std::tan(lat1))};
+  const double sigma1{std::atan2(std::tan(U1), cosa1)};
+  const double sina{std::cos(U1) * sina1};
+  const double cosaSq{1e0 - sina * sina};
+  const double uSq{cosaSq * (a * a - b * b) / (b * b)};
+  const double k1{(std::sqrt(1e0 + uSq) - 1e0) / (std::sqrt(1e0 + uSq) + 1e0)};
+  const double A{(1e0 + 0.25e0 * k1 * k1) / (1e0 - k1)};
+  const double B{k1 * (1e0 - (3e0 / 8e0) * k1 * k1)};
+
+  double sigma{s / b * A}; // initial guess
+  double sigmaP, sigmaM2, cosSigmaM2, deltaSigma, sinSigma;
+
+  do {
+    sigmaM2 = 2e0 * sigma1 + sigma;
+    cosSigmaM2 = std::cos(sigmaM2);
+    sinSigma = std::sin(sigma);
+    deltaSigma =
+        B * sinSigma *
+        (cosSigmaM2 +
+         (1e0 / 4e0) * B *
+             (std::cos(sigma) * (-1e0 + 2e0 * cosSigmaM2 * cosSigmaM2) -
+              (1e0 / 6e0) * B * cosSigmaM2 *
+                  (-3e0 + 4e0 * sinSigma * sinSigma) *
+                  (-3e0 + 4e0 * cosSigmaM2 * cosSigmaM2)));
+    sigmaP = sigma;
+    sigma = s / (b * A) + deltaSigma;
+  } while (std::abs(sigma - sigmaP) > convergence_limit &&
+           ++iteration < MAX_ITERATIONS);
+  if (iteration >= MAX_ITERATIONS) {
+    throw std::out_of_range(
+        "[ERROR] Direct Vincenty cannot converge after 100 iterations!");
+  }
+
+  const double sinU1{std::sin(U1)};
+  const double cosU1{std::cos(U1)};
+  const double cosSigma{std::cos(sigma)};
+
+  // compute latitude
+  double nom{sinU1 * cosSigma + cosU1 * sinSigma * cosa1};
+  double denom{sina * sina +
+               std::pow(sinU1 * sinSigma - cosU1 * cosSigma * cosa1, 2e0)};
+  denom = std::sqrt(denom) * (1e0 - f);
+  lat2 = std::atan2(nom, denom);
+
+  // compute longtitude
+  nom = sinSigma * sina1;
+  denom = cosU1 * cosSigma - sinU1 * sinSigma * cosa1;
+  double lambda{std::atan2(nom, denom)};
+  double C{(f / 16e0) * cosaSq * (4e0 + f * (4e0 - 3e0 * cosaSq))};
+  double L{lambda -
+           (1e0 - C) * f * sina *
+               (sigma +
+                C * sinSigma *
+                    (cosSigmaM2 +
+                     C * cosSigma * (-1e0 + 2e0 * cosSigmaM2 * cosSigmaM2)))};
+  lon2 = L + lon1;
+
+  // compute azimouth
+  return std::atan2(sina, -sinU1 * sinSigma + cosU1 * cosSigma * cosa1) + DPI;
+}
+
+/// @brief Direct Vincenty formula.
+///
+/// Given an initial point (lat1, lon1), an initial azimuth, a1, and a distance,
+/// s, along the geodesic the problem is to find the end point (lat2, lon2)
+/// and azimuth, a2.
+/// Vincenty’s solution for the distance between points on an ellipsoidal earth
+/// model is accurate to within 0.5 mm distance (!), 0.000015″ bearing, on the
+/// ellipsoid being used. Calculations based on a spherical earth model, such
+/// as the (much simpler) Haversine, are accurate to around 0.3% – which is
+/// still good enough for many (most?) purposes, of course (see
+/// https://www.movable-type.co.uk/scripts/latlong-vincenty.html).
+///
+/// @param[in]  lat1        Latitude of point 1 (radians)
+/// @param[in]  lon1        Longtitude of point 1 (radians)
+/// @param[in]  a1          Azimouth from point 1 to point 2 (radians)
+/// @param[in]  s           Ellipsoid distance (along the geodesic) from point
+///                         1 to point 2 (meters)
+/// @param[in]  semi_major  Semi-major axis of reference ellipsoid (meters)
+/// @param[in]  semi_minor  Semi-minor axis of reference ellipsoid (meters)
+/// @param[in]  flattening  Flattening of reference ellipsoid
+/// @param[out] lat2        Latitude of point 2 (radians)
+/// @param[out] lon2        Longtitude of point 2 (radians)
+/// @param[in]  convergence_limit Convergence limit (radians). 10e−12
+///                         corresponds to approximately 0.06mm
+/// @return     Azimouth from point 2 to point 1 (radians)
+///
+/// @see https://www.movable-type.co.uk/scripts/latlong-vincenty.html
+double ngpt::core::direct_vincenty2(double lat1, double lon1, double a1,
+                                    double s, double semi_major,
+                                    double flattening, double semi_minor,
+                                    double &lat2, double &lon2,
+                                    double convergence_limit) {
+  const int MAX_ITERATIONS = 100;
+  int iteration = 0;
+
+  double a = semi_major;
+  double f = flattening;
+  double b = semi_minor;
+
+  const double cosa1{std::cos(a1)};
+  const double sina1{std::sin(a1)};
+  const double tanU1 = (1e0 - f) * std::tan(lat1);
+  const double cosU1 = 1e0 / std::sqrt((1e0 + tanU1 * tanU1));
+  const double sinU1 = tanU1 * cosU1;
+  const double sigma1 = std::atan2(
+      tanU1,
+      cosa1); // σ1 = angular distance on the sphere from the equator to P1
+  const double sina = cosU1 * sina1;
+  const double cosSqa = 1e0 - sina * sina;
+  const double uSq = cosSqa * (a * a - b * b) / (b * b);
+  const double A =
+      1e0 +
+      uSq / 16384e0 * (4096e0 + uSq * (-768e0 + uSq * (320e0 - 175e0 * uSq)));
+  const double B =
+      uSq / 1024e0 * (256e0 + uSq * (-128e0 + uSq * (74e0 - 47e0 * uSq)));
+  double sigma = s / (b * A), sinSigma = 0e0, cosSigma = 0e0,
+         deltaSigma = 0e0;  // σ = angular distance P₁ P₂ on the sphere
+  double cos2sigma_m = 0e0; // σₘ = angular distance on the sphere from the
+                            // equator to the midpoint of the line
+  double sigma_t = 0e0;
+  do {
+    cos2sigma_m = std::cos(2e0 * sigma1 + sigma);
+    sinSigma = std::sin(sigma);
+    cosSigma = std::cos(sigma);
+    deltaSigma =
+        B * sinSigma *
+        (cos2sigma_m +
+         B / 4e0 *
+             (cosSigma * (-1e0 + 2e0 * cos2sigma_m * cos2sigma_m) -
+              B / 6e0 * cos2sigma_m * (-3e0 + 4e0 * sinSigma * sinSigma) *
+                  (-3e0 + 4e0 * cos2sigma_m * cos2sigma_m)));
+    sigma_t = sigma;
+    sigma = s / (b * A) + deltaSigma;
+  } while (std::abs(sigma - sigma_t) > convergence_limit &&
+           ++iteration < MAX_ITERATIONS);
+
+  if (iteration >= MAX_ITERATIONS) {
+    throw std::out_of_range(
+        "[ERROR] Direct Vincenty cannot converge after 100 iterations!");
+  }
+
+  const double x = sinU1 * sinSigma - cosU1 * cosSigma * cosa1;
+  lat2 = std::atan2(sinU1 * cosSigma + cosU1 * sinSigma * cosa1,
+                    (1e0 - f) * std::sqrt(sina * sina + x * x));
+  const double lambda =
+      std::atan2(sinSigma * sina1, cosU1 * cosSigma - sinU1 * sinSigma * cosa1);
+  const double C = f / 16e0 * cosSqa * (4e0 + f * (4e0 - 3e0 * cosSqa));
+  const double L =
+      lambda - (1e0 - C) * f * sina *
+                   (sigma + C * sinSigma *
+                                (cos2sigma_m +
+                                 C * cosSigma *
+                                     (-1e0 + 2e0 * cos2sigma_m * cos2sigma_m)));
+  lon2 = lon1 + L;
+
+  const double a2 = std::atan2(sina, -x);
+  return std::fmod(a2 + ngpt::D2PI, D2PI);
+}
---
+++ test/test_geodtest.cpp
@@ -0,0 +1,148 @@
+#include "geodesy.hpp"
+#include "test_help.hpp"
+#include "vincenty.hpp"
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <fstream>
+#include <iostream>
+#include <random>
+
+using namespace ngpt;
+
+/// constants for wgs84 ellipsoid
+constexpr double A = ellipsoid_traits<ellipsoid::wgs84>::a,
+                 F = ellipsoid_traits<ellipsoid::wgs84>::f,
+                 B = semi_minor<ellipsoid::wgs84>();
+
+/// transform radians to seconds (of degrees)
+double rad2seconds(double rad) noexcept {
+  double ddeg = rad2deg(rad);
+  return ddeg * 3600e0;
+}
+
+/// hold (absolute) max error values in seconds
+double max_error_lat = std::numeric_limits<double>::min(),
+       max_error_lon = std::numeric_limits<double>::min(),
+       max_error_az = std::numeric_limits<double>::min();
+double lat_at1, lat_at2, lat_at3;
+
+/// struct to hold each line of the GeodTest file
+/// see https://zenodo.org/record/32156
+struct TestLine {
+  // lat1[0], lon1[1], az1[2], 
+  // lat2[3], lon2[4], az2[5], 
+  // s12[6],  arc[7],  rs12[8], 
+  // t[9]
+  double ar[10];
+
+  void test_vincenty_direct(bool print = true) const {
+    double lat2, lon2, az2;
+    az2 = ngpt::core::direct_vincenty2(ar[0], ar[1], ar[2], ar[6], A, F, B,
+                                       lat2, lon2);
+    if (print)
+      printf("\ndlat=%20.15f dlon=%20.15f dAz=%20.15f s=%15.3f",
+             rad2seconds(ar[3] - lat2), rad2seconds(ar[4] - lon2),
+             rad2seconds(ar[5] - az2), ar[6] * 1e-3);
+    if (std::abs(rad2seconds(ar[3] - lat2)) > max_error_lat) {
+      max_error_lat = std::abs(rad2seconds(ar[3] - lat2));
+      lat_at1 = ar[3];
+    }
+    if (std::abs(rad2seconds(ar[4] - lon2)) > max_error_lon) {
+      max_error_lon = std::abs(rad2seconds(ar[4] - lon2));
+      lat_at2 = ar[3];
+    }
+    if (std::abs(rad2seconds(ar[5] - az2)) > max_error_az) {
+      max_error_az = std::abs(rad2seconds(ar[5] - az2));
+      lat_at3 = ar[3];
+    }
+    return;
+  }
+};
+
+/// resolve a GeodTest line to a TestLine instance
+int from_line(const char *line, TestLine &tl) {
+  char *end;
+  for (int i = 0; i < 10; i++) {
+    tl.ar[i] = std::strtod(line, &end);
+    assert(end != line);
+    if (errno == ERANGE) {
+      std::cerr << "\n[ERROR] Failed to decode line: \"" << line << "\"";
+      return 1;
+    }
+    line = end;
+  }
+  for (int i = 0; i < 6; i++)
+    tl.ar[i] = deg2rad(tl.ar[i]);
+  tl.ar[7] = deg2rad(tl.ar[7]);
+  return 0;
+}
+
+/// various constants
+const char *GeodTest = "/home/xanthos/Builds/ggeodesy/test/GeodTest.dat";
+constexpr std::size_t MAX_CHARS = 256;
+using szstr_pair = std::pair<std::size_t, std::string>;
+const std::vector<szstr_pair> GeodTest_descr = {
+    {100000, "randomly distributed"},
+    {50000, "nearly antipodal"},
+    {50000, "short distances"},
+    {50000, "one end near a pole"},
+    {50000, "both ends near opposite poles"},
+    {50000, "nearly meridional"},
+    {50000, "nearly equatorial"},
+    {50000, "running between vertices (a1 = a2 = 90deg)"},
+    {50000, "ending close to vertices"}};
+const std::size_t total_lines =
+    std::accumulate(GeodTest_descr.begin(), GeodTest_descr.end(), 0,
+                    [](const std::size_t &sum, const szstr_pair &pair) {
+                      return sum + pair.first;
+                    });
+// assert(total_lines == 500000);
+
+int main() {
+
+  std::ifstream fin(GeodTest);
+  if (!fin.is_open()) {
+    std::cerr << "\n[ERROR] Failed to open file \"" << GeodTest << "\"";
+    return 1;
+  }
+
+  std::size_t line_nr = 0, line_count = 0;
+  char line[MAX_CHARS];
+  std::vector<TestLine> vec;
+  TestLine tl;
+
+  for (const auto &batch : GeodTest_descr) {
+    line_nr = 0;
+    vec.clear();
+    vec.reserve(batch.first);
+    max_error_lat = std::numeric_limits<double>::min();
+    max_error_lon = std::numeric_limits<double>::min();
+    max_error_az = std::numeric_limits<double>::min();
+    do {
+      fin.getline(line, MAX_CHARS);
+      if (!fin.good() || from_line(line, tl)) {
+        return 1;
+      }
+      vec.emplace_back(tl);
+    } while (++line_nr < batch.first);
+    line_count += line_nr;
+    for (const auto &ar : vec) {
+      ar.test_vincenty_direct(false);
+    }
+    std::cout << "\n>> " << batch.second;
+    std::cout
+        << "\n-------------------------------------------------------------";
+    printf("\nMax difs: lat:%20.15fsec lon:%20.15fsec az:%20.15fsec lat:%15.4f(m) lon:%15.4f(m)", max_error_lat,
+           max_error_lon, max_error_az,
+           infinitesimal_meridian_arc<ellipsoid::wgs84, double>(
+               lat_at1, deg2rad(max_error_lat / 3600e0)),
+           parallel_arc_length<ellipsoid::wgs84, double>(
+               lat_at2, deg2rad(max_error_lon / 3600e0)));
+  }
+  printf("\nNumber of lines read: %15d", (int)line_count);
+
+  printf("\n");
+  return 0;
+}
