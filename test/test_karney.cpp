7e7c8f5e3b61aa1b76ba82035c3fbbb4167eef09
xanthos <xanthos.papanikolaou@gmail.com>
2020-12-01 02:22:40 +0200

    remove boost/geometry from test

--- test/test_karney.cpp
+++ test/test_karney.cpp
@@ -8,10 +8,7 @@
 #include <chrono>
 #include <iostream>
 #include <random>
-#include <boost/geometry.hpp>
-#include <boost/geometry/formulas/karney_direct.hpp>
 
-using namespace boost::geometry;
 using namespace ngpt;
 
 int main() {
@@ -25,25 +22,14 @@ constexpr double A = ellipsoid_traits<ellipsoid::wgs84>::a,
   const double a1=30e0;
   const double s=10000000e0;
   double lat2, lon2;
-  ngpt::core::direct_karney(lat1, lon1, a1, s, A, F, B, lat2, lon2);
+  double a2 = ngpt::core::direct_karney(lat1, lon1, a1, s, A, F, B, lat2, lon2);
 
-// For storing the resulting values.
-formula::result_direct<double> result_drc;
-
-// WGS-84 spheroid.
-srs::spheroid<double> spheroid(A, B);
-
-// Define the strategy.
-typedef formula::karney_direct<double, true, true, false, false, 6u>
-    karney_direct_type;
-result_drc =
-        karney_direct_type::apply(lon1, lat1, s, a1, spheroid);
 //printf("\nLatitude  : %20.11f",rad2deg(result_drc.lat2));
 //printf("\nLongtitude: %20.11f",rad2deg(result_drc.lon2));
 //printf("\nAzimuth   : %20.11f",rad2deg(result_drc.reverse_azimuth));
-printf("\nLatitude  : %20.11f",(result_drc.lat2));
-printf("\nLongtitude: %20.11f",(result_drc.lon2));
-printf("\nAzimuth   : %20.11f",(result_drc.reverse_azimuth));
+printf("\nLatitude  : %20.11f",(lat2));
+printf("\nLongtitude: %20.11f",(lon2));
+printf("\nAzimuth   : %20.11f",(a2));
 
   return 0;
 }
