4d77113dc424cc1466469350742b4ceea83fd60b
xanthos <xanthos.papanikolaou@gmail.com>
2022-06-08 01:57:52 +0300

    fixed bug in car2top

--- src/car2top.cpp
+++ src/car2top.cpp
@@ -1,11 +1,12 @@
 #include "geodesy.hpp"
+#include <cstdio>
 
 using dso::MATRIX3x3;
 using dso::VECTOR3;
 
-void dso::core::dcar2top(double xi, double yi, double zi, double dx, double dy, double dz,
-              double semi_major, double flattening, double &east, double &north,
-              double &up) noexcept {
+void dso::core::dcar2top(double xi, double yi, double zi, double dx, double dy,
+                         double dz, double semi_major, double flattening,
+                         double &east, double &north, double &up) noexcept {
 
   // Ellipsoidal coordinates of reference point.
   double phi_i, lambda_i, h_i;
@@ -28,26 +29,10 @@ void dso::core::dcar2top(double xi, double yi, double zi, double dx, double dy, 
   return;
 }
 
-VECTOR3 dso::core::dcar2top(const VECTOR3 &r, const VECTOR3 &dr, double semi_major,
-                 double flattening) noexcept {
+VECTOR3 dso::core::dcar2top(const VECTOR3 &r, const VECTOR3 &dr,
+                            double semi_major, double flattening) noexcept {
 
   // Cartesian to ellipsoidal for reference point.
   const VECTOR3 lfh = dso::car2ell(r, semi_major, flattening);
-
-  // Trigonometric numbers.
-  //const double cosf{std::cos(lfh.y())};
-  //const double sinf{std::sin(lfh.y())};
-  //const double sinl{std::sin(lfh.x())};
-  //const double cosl{std::cos(lfh.x())};
-
-  //// Topocentric vector.
-  //const double north =
-  //    -sinf * cosl * dr.x() - sinf * sinl * dr.y() + cosf * dr.z();
-  //const double east = -sinl * dr.x() + cosl * dr.y();
-  //const double up = cosf * cosl * dr.x() + cosf * sinl * dr.y() + sinf * dr.z();
-
-  // Finished.
-  // return Vector3({east, north, up});
-
-  return dso::topocentric_matrix(lfh) * dr;
+  return dso::topocentric_matrix(lfh).transpose() * dr;
 }
