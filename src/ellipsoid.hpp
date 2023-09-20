b41ff7af5fb5c06c3bba112b71d80a0a1b603cbe
xanthos <xanthos@mail.ntua.gr>
2023-09-20 15:05:13 +0300

    cleanup

--- src/ellipsoid.hpp
+++ src/ellipsoid.hpp
@@ -1,39 +1,38 @@
-/// @file ellipsoid.hpp
-/// @brief Definition and basic setup of an ellipsoid (kinda) class.
-///
-/// This file defines a list of frequently used (referennce) Ellipsoids in
-/// geodesy. Each ellipsoid comes with a list of fundamental geometric
-/// characteristics (semi-major axis, flattening, name).
-///
-/// There are two ways a user can make use of the ellipsoids:
-/// * 1. If the ellipsoid of choice is known at compile time, then the
-///      template function/traits can be used. A (per-ellipsoid) specialized
-///      traits class (i.e. dso::ellipsoid_traits) is used to implement the
-///      basic properties of each ellipsoid.
-///      E.g., it the ellipsoid of choice is GRS80, then:
-///      double semi_major = ellipsoid_traits<ellipsoid::grs80>::a;
-///      or
-///      double N = N<ellipsoid::grs80>(lat);
-///      gives the normal radius of curvature at latitude lat.
-///
-/// * 2. If the ellipsoid of choice is only kown at runtime, then all relevant
-///      computations/constants can be accessed via the Ellipsoid class; i.e.
-///      Ellipsoid e (ellipsoid::grs80);
-///      double semi_major = e.semi_major();
-///      double N = e.N(lat);
-/// Note that the semi-major axis is sometimes reffered to as
-/// "equatorial radius" and the semi-minor axis is sometimes reffered to as
-/// "polar radius".
-/// In the following and when no other qualification is used, latitude is used
-/// for Geodetic latitude.
-///
-/// @example test_ellipsoid.cpp
-///
-/// [1] H. Moritz, GEODETIC REFERENCE SYSTEM 1980,
-/// https://geodesy.geology.ohio-state.edu/course/refpapers/00740128.pdf
-///
-/// [2] Charles F. F. Karney, Algorithms for geodesics, J Geod (2013) 87:43–55
-/// [3] https://en.wikipedia.org/wiki/Latitude
+/* @file ellipsoid.hpp
+ * Definition and basic setup of an ellipsoid (kinda) class.
+ *
+ * This file defines a list of frequently used (reference) Ellipsoids in
+ * geodesy. Each ellipsoid comes with a list of fundamental geometric
+ * characteristics (semi-major axis, flattening, name).
+ *
+ * There are two ways a user can make use of the ellipsoids:
+ * * 1. If the ellipsoid of choice is known at compile time, then the
+ *      template function/traits can be used. A (per-ellipsoid) specialized
+ *      traits class (i.e. dso::ellipsoid_traits) is used to implement the
+ *      basic properties of each ellipsoid.
+ *      E.g., it the ellipsoid of choice is GRS80, then:
+ *      double semi_major = ellipsoid_traits<ellipsoid::grs80>::a;
+ *      or
+ *      double N = N<ellipsoid::grs80>(lat);
+ *      gives the normal radius of curvature at latitude lat.
+ *
+ * * 2. If the ellipsoid of choice is only kown at runtime, then all relevant
+ *      computations/constants can be accessed via the Ellipsoid class; i.e.
+ *      Ellipsoid e (ellipsoid::grs80);
+ *      double semi_major = e.semi_major();
+ *      double N = e.N(lat);
+ * Note that the semi-major axis is sometimes reffered to as 
+ * "equatorial radius" and the semi-minor axis is sometimes reffered to as
+ * "polar radius".
+ * In the following and when no other qualification is used, latitude is used
+ * for Geodetic latitude.
+ *
+ * References:
+ * [1] H. Moritz, GEODETIC REFERENCE SYSTEM 1980,
+ * https://geodesy.geology.ohio-state.edu/course/refpapers/00740128.pdf
+ * [2] Charles F. F. Karney, Algorithms for geodesics, J Geod (2013) 87:43–55
+ * [3] https://en.wikipedia.org/wiki/Latitude
+ */
 
 #ifndef __REFERENCE_ELLIPSOID_HPP__
 #define __REFERENCE_ELLIPSOID_HPP__
@@ -43,84 +42,85 @@
 
 namespace dso {
 
-/// @brief A list of well-known reference ellipsoids.
-///
-/// For each of these reference ellipsoids, a series of traits (i.e. geometric
-/// characteristics) will be specialized later on, using the template class
-/// dso::ellipsoid_traits.
-///
+/* @brief A list of well-known reference ellipsoids.
+ *
+ * For each of these reference ellipsoids, a series of traits (i.e. geometric
+ * characteristics) will be specialized later on, using the template class
+ * dso::ellipsoid_traits.
+ */
 enum class ellipsoid : char { grs80, wgs84, pz90 };
 
-/// @brief A class to hold ellipsoid traits (generic case).
-///
-/// A (template class) to hold specialized geometric quantities for each
-/// of the eumerated elements (i.e. reference ellipsoids) in the
-/// dso::ellipsoid enum.
-/// I.e., to make any element of dso::ellipsoid usable, specialize this
-/// (trait) class.
-///
-/// @tparam E  The reference ellipsoid to be specialized (i.e. one of
-///            dso::ellipsoid).
+/* @brief A class to hold ellipsoid traits (generic case).
+ *
+ * A (template class) to hold specialized geometric quantities for each
+ * of the eumerated elements (i.e. reference ellipsoids) in the
+ * dso::ellipsoid enum.
+ * I.e., to make any element of dso::ellipsoid usable, specialize this
+ * (trait) class.
+ *
+ * @tparam E  The reference ellipsoid to be specialized (i.e. one of
+ *            dso::ellipsoid).
+ */
 template <ellipsoid E> struct ellipsoid_traits {};
 
-/// @brief A class to hold traits for the GRS-80 (i.e. dso::ellispoid::grs80)
-/// reference ellipsoid.
-///
-/// @see https://en.wikipedia.org/wiki/GRS_80
+/* @brief A class to hold traits for the GRS-80 (i.e. dso::ellispoid::grs80)
+ * reference ellipsoid.
+ * @see https://en.wikipedia.org/wiki/GRS_80
+ */
 template <> struct ellipsoid_traits<ellipsoid::grs80> {
-  /// Semi-major axis (m).
+  /* Semi-major axis (m). */
   static constexpr double a{6378137e0};
-  /// Flattening.
+  /* Flattening. */
   static constexpr double f{1e0 / 298.257222101e0};
-  /// Reference ellipsoid name.
+  /* Reference ellipsoid name. */
   static constexpr const char *n{"GRS80"};
 };
 
-/// @brief A class to hold traits for the WGS-84 (i.e. dso::ellispoid::wgs84)
-/// reference ellipsoid.
-///
-/// @see https://en.wikipedia.org/wiki/World_Geodetic_System
+/* @brief A class to hold traits for the WGS-84 (i.e. dso::ellispoid::wgs84)
+ * reference ellipsoid.
+ * @see https://en.wikipedia.org/wiki/World_Geodetic_System
+ */
 template <> struct ellipsoid_traits<ellipsoid::wgs84> {
-  /// Semi-major axis (m).
+  /* Semi-major axis (m). */
   static constexpr double a{6378137e0};
-  /// Flattening.
+  /* Flattening. */
   static constexpr double f{1e0 / 298.257223563e0};
-  /// Reference ellipsoid name.
+  /* Reference ellipsoid name. */
   static constexpr const char *n{"WGS84"};
 };
 
-/// @brief A class to hold traits for the PZ-90 (i.e. dso::ellispoid::pz90)
-/// reference ellipsoid.
-///
-/// @see
-/// http://www.navipedia.net/index.php/Reference_Frames_in_GNSS#GLONASS_reference_frame_PZ-90
+/* @brief A class to hold traits for the PZ-90 (i.e. dso::ellispoid::pz90)
+ * reference ellipsoid.
+ * @see
+ * http://www.navipedia.net/index.php/Reference_Frames_in_GNSS#GLONASS_reference_frame_PZ-90
+ */
 template <> struct ellipsoid_traits<ellipsoid::pz90> {
-  /// Semi-major axis (m).
+  /* Semi-major axis (m). */
   static constexpr double a{6378136e0};
-  /// Flattening.
+  /* Flattening. */
   static constexpr double f{1e0 / 298.257839303e0};
-  /// Reference ellipsoid name.
+  /* Reference ellipsoid name. */
   static constexpr const char *n{"PZ90"};
 };
 
-/// @brief Compute the squared eccentricity.
-///
-/// @tparam E  The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @return    Eccentricity squared.
-/// @throw     Does not throw.
-///
-/// @see dso::core::eccentricity_squared
+/* @brief Compute the squared eccentricity.
+ *
+ * @tparam E  The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @return    Eccentricity squared.
+ *
+ * @see dso::core::eccentricity_squared
+ */
 template <ellipsoid E> constexpr double eccentricity_squared() noexcept {
   return core::eccentricity_squared(ellipsoid_traits<E>::f);
 }
 
-/// @brief Compute the linear eccentricity.
-///
-/// @tparam E  The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @return    linear eccentricity
-/// @throw     Does not throw.
-///
-/// @see dso::core::linear_eccentricity
+/* @brief Compute the linear eccentricity.
+ *
+ * @tparam E  The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @return    linear eccentricity
+ *
+ * @see dso::core::linear_eccentricity
+ */
 template <ellipsoid E>
 #if defined(__GNUC__) && !defined(__llvm__)
 constexpr
@@ -131,67 +131,65 @@ constexpr
                                    ellipsoid_traits<E>::f);
 }
 
-/// @brief Compute the semi-minor axis (b).
-///
-/// @tparam E The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @return   Semi-minor axis of the reference ellipsoid in meters.
-///
-/// @see dso::core::semi_minor
+/* @brief Compute the semi-minor axis (b).
+ *
+ * @tparam E The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @return   Semi-minor axis of the reference ellipsoid in [m].
+ *
+ * @see dso::core::semi_minor
+ */
 template <ellipsoid E> constexpr double semi_minor() noexcept {
   return core::semi_minor(ellipsoid_traits<E>::a, ellipsoid_traits<E>::f);
 }
 
-/// @brief Compute the polar radius of curvature (c).
-///
-/// @tparam E The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @return   Polar radius of curvature of the reference ellipsoid in meters.
-///
-/// @see dso::core::polar_radius_of_curvature
+/* @brief Compute the polar radius of curvature (c).
+ *
+ * @tparam E The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @return   Polar radius of curvature of the reference ellipsoid in [m].
+ *
+ * @see dso::core::polar_radius_of_curvature
+ */
 template <ellipsoid E> constexpr double polar_radius_of_curvature() noexcept {
   return core::polar_radius_of_curvature(ellipsoid_traits<E>::a,
                                          ellipsoid_traits<E>::f);
 }
 
-/// @brief Compute the third flattening
-///
-/// @tparam E The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @return   The third flattening
-///
-/// @see dso::core::third_flattening
+/* @brief Compute the third flattening
+ *
+ * @tparam E The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @return   The third flattening
+ *
+ * @see dso::core::third_flattening
+ */
 template <ellipsoid E> constexpr double third_flattening() noexcept {
   return core::third_flattening(ellipsoid_traits<E>::f);
 }
 
-/// @brief Compute the normal radius of curvature at a given latitude (on a
-///        reference ellipsoid).
-///
-/// @param[in] lat The latitude in radians.
-/// @tparam    E   The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @return        The normal radius of curvature in meters.
-//
-/// @see dso::core::N
+/* @brief Compute the normal radius of curvature at a given latitude (on a
+ *        reference ellipsoid).
+ *
+ * @param[in] lat The latitude in [rad].
+ * @tparam    E   The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @return        The normal radius of curvature in [m].
+ *
+ * @see dso::core::N
+ */
 template <ellipsoid E> double N(double lat) noexcept {
   return core::N(lat, ellipsoid_traits<E>::a, semi_minor<E>());
 }
 
-/// @brief Same as above, but also returns the computed trigonometric numbers
-template <ellipsoid E>
-double N(double lat, double &coslat, double &sinlat) noexcept {
-  return core::N(lat, ellipsoid_traits<E>::a, semi_minor<E>(), coslat, sinlat);
-}
-
-/// @brief Compute the geocentric latitude at some geodetic latitude on the
-///        ellipsoid (that is ellipsoidal height = 0).
-///
-/// @tparam    E   The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @param[in] lat The geodetic latitude [rad]
-/// @return        The geocentric latitude [rad]
-/// @throw     Does not throw.
-///
-/// @note If the point has height != 0 (aka, is not ON the ellipsoid), the use
-///       the version whilch also takes height as input.
-///
-/// @see dso::core::geocentric_latitude
+/* @brief Compute the geocentric latitude at some geodetic latitude on the
+ *        ellipsoid (that is ellipsoidal height = 0).
+ *
+ * @tparam    E   The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @param[in] lat The geodetic latitude [rad]
+ * @return        The geocentric latitude [rad]
+ *
+ * @note If the point has height != 0 (aka, is not ON the ellipsoid), the use
+ *       the version whilch also takes height as input.
+ *
+ * @see dso::core::geocentric_latitude
+ */
 template <ellipsoid E>
 #if defined(__GNUC__) && !defined(__llvm__)
 constexpr
@@ -201,24 +199,24 @@ constexpr
   return core::geocentric_latitude(lat, ellipsoid_traits<E>::f);
 }
 
-/// @brief Compute the geocentric latitude at some geodetic latitude, for a
-///        point with a given ellipsoidal height.
-///
-/// @tparam    E   The reference ellipsoid (i.e. one of dso::ellipsoid).
-/// @param[in] lat The geodetic latitude [rad]
-/// @param[in] h   The ellipsoidal height [m]
-/// @return        The geocentric latitude [rad]
-/// @throw     Does not throw.
-///
-/// @note If the point has height = 0 (aka, is ON the ellipsoid), then use
-///       the version whilch does not take height as input. The two functions, 
-///       converge to more than 1e-15 if you set here h=0e0 as input parameter.
-///       See the test in test/test_ellipsoidal_to_geocentric_latitude.cpp
-///
-/// @see dso::core::geocentric_latitude
-/// @see https://www.mathworks.com/help/aeroblks/geodetictogeocentriclatitude.html
-///      note that in the polar axis distance there is a sin when it should 
-///      have been a cos.
+/* @brief Compute the geocentric latitude at some geodetic latitude, for a
+ *        point with a given ellipsoidal height.
+ *
+ * @tparam    E   The reference ellipsoid (i.e. one of dso::ellipsoid).
+ * @param[in] lat The geodetic latitude [rad]
+ * @param[in] h   The ellipsoidal height [m]
+ * @return        The geocentric latitude [rad]
+ *
+ * @note If the point has height = 0 (aka, is ON the ellipsoid), then use
+ *       the version whilch does not take height as input. The two functions, 
+ *       converge to more than 1e-15 if you set here h=0e0 as input parameter.
+ *       See the test in test/test_ellipsoidal_to_geocentric_latitude.cpp
+ *
+ * @see dso::core::geocentric_latitude
+ * @see https://www.mathworks.com/help/aeroblks/geodetictogeocentriclatitude.html
+ *      note that in the polar axis distance there is a sin when it should 
+ *      have been a cos.
+ */
 template <ellipsoid E>
 #if defined(__GNUC__) && !defined(__llvm__)
 constexpr
@@ -226,8 +224,7 @@ constexpr
     double
     geocentric_latitude(double lat, double h) noexcept {
   constexpr const double ecc2 = eccentricity_squared<E>();
-  double slat;
-  double clat;
+  double slat, clat;
   const double _N = N<E>(lat, clat, slat);
   const double rho = (_N + h) * clat;
   const double z = (_N * (1e0 - ecc2) + h) * slat;
--- src/ellipsoid_core.hpp
+++ src/ellipsoid_core.hpp
@@ -1,7 +1,6 @@
-/// @file ellipsoid_core.hpp
-/// @brief Core function of ellipsoidal geometry
-/// [1] H. Moritz, GEODETIC REFERENCE SYSTEM 1980,
-/// https://geodesy.geology.ohio-state.edu/course/refpapers/00740128.pdf
+/* @file
+ * Define core functions of ellipsoidal geometry.
+ */
 
 #ifndef __ELLIPSOID_GEOMETRY_CORE_HPP__
 #define __ELLIPSOID_GEOMETRY_CORE_HPP__
@@ -10,48 +9,55 @@
 
 namespace dso {
 
-/// @brief core namespace holds the core of ellipsoid-related functions
 namespace core {
 
-/// @brief Compute the squared eccentricity.
-///
-/// Compute the squared (first) eccentricity (i.e. \f$e^2\f$) given the
-/// flattening of an ellipsoid, aka \f$ e^2 = \frac{a^2-b^2}{a^2} = (2-f)*f \f$
-/// @param[in] f flattening
-/// @return      squared eccentricity
+/* @brief Compute the squared eccentricity.
+ *
+ * Compute the squared (first) eccentricity (i.e. \f$e^2\f$) given the
+ * flattening of an ellipsoid, aka \f$ e^2 = \frac{a^2-b^2}{a^2} = (2-f)*f \f$
+ *
+ * @param[in] f flattening
+ * @return      squared eccentricity
+ */
 inline constexpr double eccentricity_squared(double f) noexcept {
   return (2e0 - f) * f;
 }
 
-/// @brief Compute the third flattening
-///
-/// Compute the third flattening (usually denoted as \f$ n \f$), given the
-/// flattening \f$ f \f$, aka \f$ n = \frac{a-b}{a+b} = f/(2-f) \f$.
-/// @param[in] f flattening
-/// @return third flattening \f$ n \f$
-/// Reference [2]
+/* @brief Compute the third flattening.
+ *
+ * Compute the third flattening (usually denoted as \f$ n \f$), given the
+ * flattening \f$ f \f$, aka \f$ n = \frac{a-b}{a+b} = f/(2-f) \f$.
+ * Reference [2]
+ *
+ * @param[in] f flattening
+ * @return third flattening \f$ n \f$
+ */
 inline constexpr double third_flattening(double f) noexcept {
   return f / (2e0 - f);
 }
 
-/// @brief Compute the semi-minor axis (aka \f$b\f$)
-///
-/// Compute the semi-minor axis of an ellipsoid(i.e. \f$e^2\f$), given the
-/// flattening and the semi-major axis, aka \f$ \beta = \alpha * (1-f) \f$
-/// @param[in] f flattening
-/// @param[in] a semi-major axis (meters)
-/// @return      semi-minor axis (meters)
+/* @brief Compute the semi-minor axis of an ellipsoid (aka \f$b\f$).
+ *
+ * Compute the semi-minor axis of an ellipsoid (i.e. \f$\beta\f$), given the
+ * flattening and the semi-major axis, aka \f$ \beta = \alpha * (1-f) \f$.
+ *
+ * @param[in] f flattening
+ * @param[in] a semi-major axis
+ * @return      semi-minor axis
+ */
 inline constexpr double semi_minor(double a, double f) noexcept {
   return a * (1e0 - f);
 }
 
-/// @brief the linear eccentricity
-///
-/// Compute the linear eccentricity of an ellipsoid (i.e. E) from the formula
-/// \f$ E = \sqrt{a^2 - b^2} \f$
-/// @param[in] f flattening
-/// @param[in] a semi-major axis (meters)
-/// @return      semi-minor axis (meters)
+/* @brief the linear eccentricity
+ *
+ * Compute the linear eccentricity of an ellipsoid (i.e. \f$E\f$) from the
+ * formula \f$ E = \sqrt{a^2 - b^2} \f$.
+ *
+ * @param[in] f flattening
+ * @param[in] a semi-major axis
+ * @return      linear eccentricity \f$E\f$
+ */
 inline
 #if defined(__GNUC__) && !defined(__llvm__)
     constexpr
@@ -62,102 +68,88 @@ inline
   return std::sqrt(a * a - b * b);
 }
 
-/// @brief Polar radius of curvature
-///
-/// Compute the polar radius of curvature of an ellipsoid (i.e. c) from the
-/// formula \f$ c = a^2 / b\f$
-/// @param[in] f flattening
-/// @param[in] a semi-major axis (meters)
-/// @return      semi-minor axis (meters)
+/* @brief Polar radius of curvature
+ *
+ * Compute the polar radius of curvature of an ellipsoid (i.e. \f$c\f$) from
+ * the formula \f$ c = a^2 / b\f$.
+ *
+ * @param[in] f flattening
+ * @param[in] a semi-major axis
+ * @return      radius of curvature \f$c\f$
+ */
 inline constexpr double polar_radius_of_curvature(double a, double f) noexcept {
   const double b = semi_minor(a, f);
   return a * a / b;
 }
 
-/// @brief Compute the normal radius of curvature at a given latitude (on
-///        a reference ellipsoid).
-///
-/// @param[in] lat The latitude (radians)
-/// @param[in] a   The ellipsoid's semi-major axis (meters)
-/// @param[in] b   The ellipsoid's semi-minor axis (meters)
-/// @return        The normal radius of curvature (meters)
-/// @throw         Does not throw (see notes)
-///
-/// @note If the denominator (den) is zero then funny things could happen;
-///       this however should **never** occur for any reference ellipsoid.
-///
-/// @see "Physical Geodesy", pg. 194
-/// @see https://en.wikipedia.org/wiki/Earth_radius
+/* @brief Compute the normal radius of curvature at a given latitude (on
+ *        a reference ellipsoid).
+ *
+ * References: "Physical Geodesy", pg. 194 and
+ * https://en.wikipedia.org/wiki/Earth_radius.
+ *
+ * @param[in] lat The latitude [rad]
+ * @param[in] a   The ellipsoid's semi-major axis [m]
+ * @param[in] b   The ellipsoid's semi-minor axis [m]
+ * @return        The normal radius of curvature [m]
+ *
+ * @note If the denominator (den) is zero then funny things could happen;
+ *       this however should **never** occur for any reference ellipsoid.
+ *
+ */
 inline
 #if defined(__GNUC__) && !defined(__llvm__)
     constexpr
 #endif
     double
     N(double lat, double a, double b) noexcept {
-  const double cosf{std::cos(lat)};
-  const double sinf{std::sin(lat)};
-  const double acosf{a * cosf};
-  const double bsinf{b * sinf};
-  const double den{std::sqrt(acosf * acosf + bsinf * bsinf)};
+  const double cosf = std::cos(lat);
+  const double sinf = std::sin(lat);
+  const double acosf = a * cosf;
+  const double bsinf = b * sinf;
+  const double den = std::sqrt(acosf * acosf + bsinf * bsinf);
   return (a * a) / den;
 }
 
-/// @brief Same as above, but also returned the computed trigonometric numbers
-///        (for further use)
-inline
-#if defined(__GNUC__) && !defined(__llvm__)
-    constexpr
-#endif
-    double
-    N(double lat, double a, double b, double &cosf, double &sinf) noexcept {
-  cosf=std::cos(lat);
-  sinf=std::sin(lat);
-  const double acosf{a * cosf};
-  const double bsinf{b * sinf};
-  const double den{std::sqrt(acosf * acosf + bsinf * bsinf)};
-  return (a * a) / den;
-}
-
-/// @brief Compute the meridional radii of curvature at a given latitude
-///        on a reference ellipsoid.
-///
-/// @param[in] lat The latitude in (radians)
-/// @param[in] a   The ellipsoid's semi-major axis (meters)
-/// @param[in] b   The ellipsoid's semi-minor axis (meters)
-/// @return        The meridional radius of curvature (meters)
-/// @throw         Does not throw (see notes)
-///
-/// @see https://en.wikipedia.org/wiki/Earth_radius
-///
+/* @brief Compute the meridional radii of curvature at a given latitude
+ *        on a reference ellipsoid.
+ *
+ * Reference: https://en.wikipedia.org/wiki/Earth_radius
+ *
+ * @param[in] lat The latitude in [rad]
+ * @param[in] a   The ellipsoid's semi-major axis [m]
+ * @param[in] b   The ellipsoid's semi-minor axis [m]
+ * @return        The meridional radius of curvature [m]
+ */
 inline
 #if defined(__GNUC__) && !defined(__llvm__)
     constexpr
 #endif
     double
     M(double lat, double a, double b) noexcept {
-  const double cosf{std::cos(lat)};
-  const double sinf{std::sin(lat)};
-  const double acosf{a * cosf};
-  const double bsinf{b * sinf};
-  const double tmpd{acosf * acosf + bsinf * bsinf};
+  const double cosf = std::cos(lat);
+  const double sinf = std::sin(lat);
+  const double acosf = a * cosf;
+  const double bsinf = b * sinf;
+  const double tmpd = acosf * acosf + bsinf * bsinf;
   return ((a * b) / tmpd) * ((a * b) / std::sqrt(tmpd));
 }
 
-/// @brief Compute the geocentric latitude, given a geodetic one for a point
-///        on the ellipsoid (aka, h = 0)
-///
-/// The geocentric latitude is the angle between the equatorial plane and the
-/// radius from the centre to a point on the surface. The relation between the
-/// geocentric latitude(\f$\theta\f$) and the geodetic latitude(\f$\phi\f$) is
-/// \f$ \theta (\phi) = tan^{-1} ((1-f)^2 tan(\phi)) \f$
-/// The geodetic and geocentric latitudes are equal at the equator and at the
-/// poles but at other latitudes they differ by a few minutes of arc.
-/// 
-/// @param[in] lat The (geodetic) latitude in [rad]
-/// @param[in] f   The ellipsoid's flattening [-]
-/// @return        The geocentric latitude [rad]
-///
-/// Reference Torge, 2001, Eq. 4.11
+/* @brief Compute the geocentric latitude, given a geodetic one for a point
+ *        on the ellipsoid (aka, h = 0)
+ *
+ * The geocentric latitude is the angle between the equatorial plane and the
+ * radius from the centre to a point on the surface. The relation between the
+ * geocentric latitude(\f$\theta\f$) and the geodetic latitude(\f$\phi\f$) is
+ * \f$ \theta (\phi) = tan^{-1} ((1-f)^2 tan(\phi)) \f$
+ * The geodetic and geocentric latitudes are equal at the equator and at the
+ * poles but at other latitudes they differ by a few minutes of arc.
+ * Reference Torge, 2001, Eq. 4.11
+ *
+ * @param[in] lat The (geodetic) latitude in [rad]
+ * @param[in] f   The ellipsoid's flattening [-]
+ * @return        The geocentric latitude [rad]
+ */
 inline
 #if defined(__GNUC__) && !defined(__llvm__)
     constexpr
@@ -167,17 +159,18 @@ inline
   return std::atan((1e0 - f) * (1e0 - f) * std::tan(lat));
 }
 
-/// @brief Compute the parametric or reduced latitude
-///
-/// The parametric or reduced latitude, \f$ beta \f$ is defined by the radius
-/// drawn from the centre of the ellipsoid to that point Q on the surrounding
-/// sphere (of radius a) which is the projection parallel to the Earth's axis
-/// of a point P on the ellipsoid at latitude \f$ \phi \f$
-/// @param[in] lat The (geodetic) latitude in radians
-/// @param[in] f   The ellipsoid's flattening
-/// @return        The parametric or reduced latitude at lat in radians
-///
-/// Reference Torge, 2001, Eq. 4.11
+/* @brief Compute the parametric or reduced latitude
+ *
+ * The parametric or reduced latitude, \f$ beta \f$ is defined by the radius
+ * drawn from the centre of the ellipsoid to that point Q on the surrounding
+ * sphere (of radius a) which is the projection parallel to the Earth's axis
+ * of a point P on the ellipsoid at latitude \f$ \phi \f$
+ *
+ * Reference Torge, 2001, Eq. 4.11
+ * @param[in] lat The (geodetic) latitude in [rad]
+ * @param[in] f   The ellipsoid's flattening [-]
+ * @return        The parametric or reduced latitude at lat in [rad]
+ */
 inline
 #if defined(__GNUC__) && !defined(__llvm__)
     constexpr
@@ -187,8 +180,8 @@ inline
   return std::atan((1e0 - f) * std::tan(lat));
 }
 
-} // core
+} /* namespace core */
 
-} // dso
+} /* namespace dso */
 
 #endif
